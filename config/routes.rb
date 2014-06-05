39a8820c8f853aa3315ac6b477cc1abe9b6d38f2
Prerna Jaiswal <prerna11081@iiitd.ac.in>
2014-06-05 16:12:30 +0530

    edit routs.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  #root 'welcome#index'
+
+  get "users/new"
+  root  to: 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+end
   
 
   # Example of regular route:
